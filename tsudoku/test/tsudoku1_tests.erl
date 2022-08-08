71bce4b48d46d51b53f23b636608a4164cfbd520
Fred Youhanaie <fyrlang@anydata.co.uk>
2022-08-08 15:33:05 +0430

    add new test puzzle

--- tsudoku/test/tsudoku1_tests.erl
+++ tsudoku/test/tsudoku1_tests.erl
@@ -10,6 +10,8 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+%%%-------------------------------------------------------------------
+
 -define(Puzzle_1, "test/puzzle_4x4_1.dat").
 -define(Solution_1,
         [[2, 1, 4, 3],
@@ -18,4 +20,22 @@
          [1, 2, 3, 4]]).
 
 puzzle_1_test() ->
-    ?assertEqual(tsudoku1:solve_file(?Puzzle_1), {ok, ?Solution_1}).
+    ?assertEqual(tsudoku1:solve_file(?Puzzle_1), {ok, ?Solution_1}),
+    espace:stop().
+
+%%%-------------------------------------------------------------------
+
+-define(Puzzle_2, "test/puzzle_6x6_3.dat").
+-define(Solution_2,
+        [[4, 3, 2, 5, 1, 6],
+         [6, 5, 1, 4, 3, 2],
+         [1, 4, 5, 6, 2, 3],
+         [2, 6, 3, 1, 4, 5],
+         [3, 1, 6, 2, 5, 4],
+         [5, 2, 4, 3, 6, 1]]).
+
+puzzle_2_test() ->
+    ?assertEqual(tsudoku1:solve_file(?Puzzle_2), {ok, ?Solution_2}),
+    espace:stop().
+
+%%%-------------------------------------------------------------------
